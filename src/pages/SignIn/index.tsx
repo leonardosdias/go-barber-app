6d2d42de81e6fd7fa41a53c14c12b6b0c559fa71
Leonardo Souza Dias <leonnardosouza96@gmail.com>
2020-10-11 01:41:44 -0300

    envio de formulario do app em andamento

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,103 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import React, { useCallback, useRef } from "react";
+import Icon from 'react-native-vector-icons/Feather';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import { useNavigation } from '@react-navigation/native';
+
+import { Image, ScrollView, View, KeyboardAvoidingView, Platform, TextInput } from "react-native";
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+} from "./styles";
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from "../../assets/logo.png";
+
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+
+  const navigation = useNavigation();
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu login</Title>
+            </View>
+
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Entrar
+                </Button>
+            </Form>
+
+            <ForgotPassword onPress={() => { }}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+
+          <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+            <Icon name="log-in" size={20} color="#ff9000" />
+            <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+          </CreateAccountButton>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </>
+  );
+};
+
+export default SignIn;
---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: "#312e38" },
+    }}
+    // initialRouteName="SignUp"
+  >
+    <Auth.Screen name="SignIn" component={SignIn} />
+    <Auth.Screen name="SignUp" component={SignUp} />
+  </Auth.Navigator>
+);
+
+export default AuthRoutes;
